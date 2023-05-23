0d63d325387f213c820d79734f04a82f42895d4b
mansiagarwal24 <mansiaggarwal@tothenew.com>
2023-05-23 10:39:04 +0530

    RestTemplate

--- RestTemplate/src/main/java/com/ProductConsumer/RestTemplate/ProductService.java
+++ RestTemplate/src/main/java/com/ProductConsumer/RestTemplate/ProductService.java
@@ -3,7 +3,6 @@ package com.ProductConsumer.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,7 +16,7 @@ public class ProductService {
     public ProductService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
-    public List<Product> getAllProducts(){
-        return restTemplate.exchange("http://localhost:8081/product-api", HttpMethod.POST,null,List.class).getBody();
+    public List getAllProducts(){
+        return restTemplate.exchange("http://localhost:8081/product-api", HttpMethod.GET,null,List.class).getBody();
     }
 }
